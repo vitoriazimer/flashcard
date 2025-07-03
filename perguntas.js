e848ab511813a193e7ad4bcb94c1c7eb0461fae4
Vitoria zimer <162469120+vitoriazimer@users.noreply.github.com>
2025-07-03 07:47:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma base?',
+    'é uma maquiagem de reboco'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a bandeira da Letonia?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
